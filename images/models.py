7d8c81d11fea65b92e27c3be41e7dd9fd3a91e44
Sonjuan <simset1212@gmail.com>
2023-04-27 01:41:56 +0900

    adding bookmarklet feature && Like/disLike feature

--- images/models.py
+++ images/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.conf import settings
 from django.utils.text import slugify
+from django.urls import reverse
+
 
 class Image(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL,
@@ -25,8 +27,11 @@ class Image(models.Model):
 
     def __str__(self):
         return self.title
-    
+
     def save(self, *args, **kwargs):
         if not self.slug:
             self.slug = slugify(self.title)
         super().save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse('images:detail', args=[self.id, self.slug])
